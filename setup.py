25757a016bad7c8e8f27874ace85a6d0f347b9a6
Raiyan Yahya <raiyanyahyadeveloper@gmail.com>
2023-02-24 17:23:22 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     name="dallecli",
     python_requires=">3.5",
     options={"bdist_wheel": {"universal": "1"}},
-    version=get_version(),
+    version="o.4.o",
     description="A command line application to help wrap the OpenAI Dalle api and other utilities.",
     long_description=long_description,
     long_description_content_type="text/markdown",

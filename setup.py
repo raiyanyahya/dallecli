3daabf262a7a6c214a69426de563d074765457ca
Raiyan Yahya <raiyanyahyadeveloper@gmail.com>
2023-02-24 17:22:12 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,29 +3,9 @@ from os import environ
 from setuptools import find_packages, setup
 import sys
 
-version_f = ".version"
 this_dir = abspath(dirname(__file__))
 with open(join(this_dir, "README.md"), encoding="utf-8") as file:
     long_description = file.read()
-
-
-def get_version():
-    if isfile(version_f):
-        with open(version_f) as version_file:
-            version = version_file.read().strip()
-            return version
-    elif (
-        "build" in sys.argv
-        or "egg_info" in sys.argv
-        or "sdist" in sys.argv
-        or "bdist_wheel" in sys.argv
-    ):
-        version = environ.get("VERSION", "0.0")  # Avoid PEP 440 warning
-        if "-SNAPSHOT" in version:
-            version = version.replace("-SNAPSHOT", ".0")
-        with open(version_f, "w+") as version_file:
-            version_file.write(version)
-        return version
 
 
 setup(
